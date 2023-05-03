f4750a41436a1909f7bc88088877456d145c126f
Andrew Galle <Andrew.Galle@LIVE.COM>
2023-05-02 21:59:16 -0400

    user will be able to change board size

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 
-// get requested board side from user
+// draw the board, initially at 50
+
+let padSize = 50;
+let sketchPad = document.querySelector("#sketchPad");
+
+function drawBoard(padSize) {
+
+    for (let i = 0; i < padSize; i++) {
+        let newDiv = document.createElement("div");
+            newDiv.id = "div" + i;
+            newDiv.className = "cell";
+    
+        sketchPad.appendChild(newDiv);
+    };
+};
+
+drawBoard(padSize);
+
+// Allow user to change board size
 
 let rangeSlider = document.getElementById("sliderRange");
 
@@ -8,16 +26,19 @@ let output = document.getElementById("boardSizeNumberDisplay");
 output.textContent = rangeSlider.value;
 
 rangeSlider.oninput = function () {
+
     output.textContent = this.value;
-}
 
-// draw the board
-let sketchPad = document.querySelector("#sketchPad");
-for (let i = 0; i <= 100; i++) {
-    let newDiv = document.createElement("div");
-    newDiv.id = "div" + i;
-    newDiv.className = "cell";
-    newDiv.textContent = newDiv.id;
+    //remove old board
 
-    sketchPad.appendChild(newDiv);
+    let boardCellsToRemove = document.querySelectorAll(".cell");
+    for (i = 0; i < boardCellsToRemove.length; i++) {
+        boardCellsToRemove[i].remove();
+    }
+
+    padSize = this.value;
+
+    drawBoard(padSize);
+
 };
+
